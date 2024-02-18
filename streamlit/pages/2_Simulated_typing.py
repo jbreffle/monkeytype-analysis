972628bc17c08c71c7a606fa2663aa0c53778fc7
jbreffle <78390184+jbreffle@users.noreply.github.com>
2024-02-18 10:54:57 -0500

    Clean up simulation notebooks and streamlit page

--- streamlit/pages/2_Simulated_typing.py
+++ streamlit/pages/2_Simulated_typing.py
@@ -33,66 +33,92 @@ def main():
     Home.configure_page(page_title="Simulated typing")
 
     # Data set up
-    data_df, _ = Home.load_data()
+    _, _ = Home.load_data()
 
     # Page introduction
     st.title("Simulated typing")
+    iid_url = "https://en.wikipedia.org/wiki/Independent_and_identically_distributed_random_variables"
     st.write(
-        """
+        f"""
         One important question when practicing typing is, 
         "how carefully should one try to avoid mistakes"?
         In the raw data we can see that performance (wpm) is strongly correlated with
         accuracy. But is this correlation causal?
 
-        One hypothesis is that mistakes are i.i.d. random, 
+        One hypothesis is that mistakes are [i.i.d.](<{iid_url}>), 
         and each mistake requires a fixed time to correct
         (time that otherwise would have been spent typing).
         This alone might cause the degree of correlation observed in the data.
-        Is the best approach for practicing typing to balance the probability of making
-        a mistake with the time it takes to correct it?
+        We can study the causal relationship between accuracy and wpm through 
+        simulations.
         """
     )
     st.divider()
 
     st.subheader("Simulated typing: random mistake draws")
     st.write(
-        f"""
-        One method to simulate typing is to randomly draw mistakes.
+        """
+        A simple method of simulating a typing session is to draw a random
+        number of mistakes from a Poisson distribution and then assume each of those
+        mistakes takes some random amount of time to correct.
+        The WPM and accuracy can then be calculated based on those random values.
+
+        Here we see results from 1000 such simulated trials.
+        If we assume an average performance of 60 WPM and 95\% accuracy
+        then we can reproduce the $R^2$ that is observed in the actual data
+        when we assume each mistake takes an average of 0.5 seconds to correct
+        with a standard deviation of 0.45.
         """
     )
     # TODO
     avg_wpm = 60
     avg_acc = 0.95
     n_trials = 1000
-    wpm, acc, n_mistakes = run_simple_sim(
-        avg_wpm=avg_wpm, avg_acc=avg_acc, n_trials=n_trials
-    )
+    wpm, acc, _ = run_simple_sim(avg_wpm=avg_wpm, avg_acc=avg_acc, n_trials=n_trials)
     # Plot scatter_hist of wpm and acc
     fig = plt.figure(figsize=(6, 4))
-    ax, ax_histx, ax_histy = plot.sim_scatter_hist(wpm, acc, fig=fig)
+    ax, _, _ = plot.sim_scatter_hist(wpm, acc, fig=fig)
     ax.axvline(avg_wpm, color="grey", linestyle="--", alpha=0.5)
     ax.axhline(avg_acc, color="grey", linestyle="--", alpha=0.5)
     ax.plot(np.mean(wpm), np.mean(acc), "ro")
     st.pyplot(fig, use_container_width=True, transparent=True)
-    # TODO
+    st.write(
+        """
+        The dashed grey lines show the target WPM and accuracy.
+        The red dot is the mean WPM and accuracy over all trial simulations.
+        The red line is the linear regression.
+        """
+    )
     st.divider()
 
     st.subheader("Simulated typing: Poisson process")
     st.write(
-        f"""
-        An alternative simulation method is to use a Poisson process.
+        """
+        A more complicated but more realistic simulation approach would be to simulate
+        typing behavior across time within each trial using a Poisson process.
+        
+        We see that we reproduce similar results to the simple method.
+        Here we model mistakes as a Poisson process and
+        assume each mistake takes 0.75 seconds to fix.
         """
     )
     avg_wpm = 60
     avg_acc = 0.95
-    wpm, acc, n_mistakes = run_poisson_sim(avg_wpm=avg_wpm, avg_acc=avg_acc)
+    wpm, acc, _ = run_poisson_sim(avg_wpm=avg_wpm, avg_acc=avg_acc)
     # Plot scatter_hist of wpm and acc
     fig = plt.figure(figsize=(6, 4))
-    ax, ax_histx, ax_histy = plot.sim_scatter_hist(wpm, acc, fig=fig)
+    ax, _, _ = plot.sim_scatter_hist(wpm, acc, fig=fig)
     ax.axvline(avg_wpm, color="grey", linestyle="--", alpha=0.5)
     ax.axhline(avg_acc, color="grey", linestyle="--", alpha=0.5)
     ax.plot(np.mean(wpm), np.mean(acc), "ro")
     st.pyplot(fig, use_container_width=True, transparent=True)
+    st.write(
+        """
+        The dashed grey lines show the target WPM and accuracy.
+        The red dot is the mean WPM and accuracy over all trial simulations.
+        The red line is the linear regression.
+        """
+    )
     st.divider()
 
     nb_url_1 = "https://github.com/jbreffle/monkeytype-analysis/blob/main/notebooks/3a_sim_simple.ipynb"
@@ -101,7 +127,7 @@ def main():
         f"""
         Click here 
         [./notebooks/3a_sim_simple.ipynb]({nb_url_1})
-        for the simple simulation method notebook.
+        for the simple simulation notebook.
 
         Click here
         [./notebooks/3a_sim_poisson.ipynb]({nb_url_2})
